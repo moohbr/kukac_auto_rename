cb9c1d1ac382f83a0717e332b3cdf7d98431d0e0
Matheus Araujo <moohbr@gmail.com>
2022-03-21 15:30:53 -0300

    [UPDATE] Now can rename without index.

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ root_path = get_folders("./")
 sub_path = get_folders(root_path)
 
 path = f'{root_path}/{sub_path}'
-
-rename_folders(path, sub_path)
+user_option = input('Do you want the files with index: (Type yes or no)\n')
+user_option = user_option.lower()
+rename_folders(path, sub_path, user_option)
 print("Done!")
--- toolbox.py
+++ toolbox.py
@@ -5,6 +5,14 @@ load_dotenv()
 
 DEBUG = os.getenv('DEBUG')
 
+
+def clean_data(data):
+    data = data.strip('\n')
+    data = data.split(' [', 1)
+
+    return data
+
+
 def get_folders(folder):
     sub_folders = [name for name in os.listdir(folder) if os.path.isdir(os.path.join(folder, name))]
     print('Reading...')
@@ -25,12 +33,20 @@ def get_folders(folder):
     return path
 
 
-def rename_folders(path, sub_path):
+def rename_folders(path, sub_path, user_option):
     files = os.listdir(path)
     type_file = input(f'Type the files format, like [txt, mp4, jpeg] : ')
     if DEBUG is True:
         for file in files:
             print(f'File {file}')
-    for index, file in enumerate(files):
-        os.rename(os.path.join(path, file), os.path.join(path, f'{sub_path}'
-                                                         .join([f'aula{str(int(index + 1))}-', f'.{type_file}'])))
+    if user_option == 'yes':
+        for index, file in enumerate(files):
+            os.rename(os.path.join(path, file), os.path.join(path, f'{sub_path}'
+                                                             .join([f'aula{str(int(index + 1))}-', f'.{type_file}'])))
+    if user_option == 'no':
+        for index, file in enumerate(files):
+            file_name = clean_data(file)
+            print(file_name)
+            file_name = file_name.pop(0)
+            print(file_name)
+            os.rename(os.path.join(path, file), os.path.join(path, f'{file_name}.{type_file}'))
